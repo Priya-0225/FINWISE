7d4c316d1d0a9f7f7b095c4b063c529b14008b78
Priya Radjavelou <priyaradjavelou@gmail.com>
2026-06-18 20:02:37 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,10 @@
-// Firebase Compat Version (EASY)
-
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCEiKZs-a88YDgHk9gYInC2tELts5TeBWI",
+  authDomain: "financial-advisor-app-ba4e5.firebaseapp.com",
+  projectId: "financial-advisor-app-ba4e5",
+  storageBucket: "financial-advisor-app-ba4e5.appspot.com",
+  messagingSenderId: "388509714885",
+  appId: "1:388509714885:web:0da4839bcaacf1911d23ac"
 };
 
 firebase.initializeApp(firebaseConfig);
